db400d1d8e5b57d7aaf19a5e46dd1c806df7fdce
camtu182 <oomikarinoo182@gmail.com>
2025-04-03 21:08:17 +0700

    add public folder

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // Đảm bảo lắng nghe trên 0.0.0.0 (host của Render)
+  port: env.int('PORT', 1337), // Render sẽ tự động set PORT
   app: {
     keys: env.array('APP_KEYS'),
   },
